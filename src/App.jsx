017027ccac978c0eabe1aac16513bb1fadd3b934
Idan Golshan <idang9@gmail.com>
2025-06-11 01:00:36 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <h1 className="text-2xl font-bold text-center">Sticky Notes Board</h1>
+    </div>
+  )
+}
+
+export default App
